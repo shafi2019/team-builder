1cc23a0ec79258ae2063cc3d68af3156551ca995
Shafi Masoumi <masoumi2017@icloud.com>
2019-10-09 09:17:25 -0700

    first commit

---
+++ team-builder/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState }  from 'react';
+import Form from './components/Form';
+import './App.css';
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <header>Team List</header>
+
+    </div>
+  );
+}
+
+export default App;
