6fa4472231037dea7d62866a20bb31937c74d273
Shafi Masoumi <masoumi2017@icloud.com>
2019-10-10 12:10:50 -0700

    form created

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -4,7 +4,38 @@ const MemberForm = props => {
 
   return (
     <form onSubmit={handleSubmit}>
-
+      <label htmlFor="name">Name</label>
+      <input
+        id="name"
+        value={member.name}
+        name="name"
+        type="text"
+        placeholder="Shafi Masoumi"
+        onChange={handleChange}
+      />
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        value={member.email}
+        name="email"
+        placeholder="email@example.com"
+        onChange={handleChange}
+      />
+      <label htmlFor="role">Role</label>
+      <select
+        id="role"
+        value={member.role}
+        name="role"
+        onChange={handleChange} >
+        <option>Please Select</option>
+        <option value='Full Stack Developer'>Full Stack Developer</option>
+        <option value='SoftWare Engineer'>SoftWare Engineer</option>
+        <option value='Back End Developer'>Back End Developer</option>
+        <option value='Front End Developer'>Front End Developer</option>
+        <option value='Data Analyst'>Data Analyst</option>
+        <option value='UX Designer'>UX Designer</option>
+      </select>
+      <button type="submit">Add Member</button>
     </form>
   );
 }
