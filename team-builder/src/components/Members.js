d811d1b6c8df4cf65f57ff3471baa6887408126d
Shafi Masoumi <masoumi2017@icloud.com>
2019-10-10 12:16:22 -0700

    map over data

--- team-builder/src/components/Members.js
+++ team-builder/src/components/Members.js
@@ -3,7 +3,17 @@ import React from "react";
 const Members = props => {
     console.log(props)
     return (
-        
+    <div className="member-list">
+        {props.memberList.map(member => {
+        return (
+          <div className="member" key={member.id}>
+            <h3>{member.name}</h3>
+            <p>{member.email}</p>
+            <p>{member.role}</p>
+          </div>
+          )
+        })}  
+    </div>
     );  
 }
 
